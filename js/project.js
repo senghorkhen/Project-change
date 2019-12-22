d7bf7ef4854ea5f5c75ed21e9f98f7700fb501a5
senghor.khen <senghor.khen@PNC.LAN>
2019-12-22 11:07:22 +0700

    Update Project

--- js/project.js
+++ js/project.js
@@ -1,14 +1,32 @@
-function getUrl() {
-    var url = "https://raw.githubusercontent.com/radytrainer/test-api/master/test.json";
-    return url;
-}
 $(document).ready(function () {
     getApi();
     $('#recipe').on('change', function () {
         var rcipeId = $('#recipe').val();
         eachRecipe(rcipeId);
+
     });
 });
+
+// function click on icon plus
+$("#add").on('click', function () {
+    var member = $("#member_project").val();
+    add(member);
+    var seclect = $('#recipe').val();
+    updateRecipe(seclect,member);
+});
+
+// function click on icon minus
+$("#minus").on('click', function () {
+    var member = $("#member_project").val();
+    minus(member);
+    var seclect = $('#recipe').val();
+    updateRecipe(seclect,member);
+});
+
+function getUrl() {
+    var url = "https://raw.githubusercontent.com/radytrainer/test-api/master/test.json";
+    return url;
+}
 
 // gteApi
 function getApi() {
@@ -19,6 +37,7 @@ function getApi() {
         error: () => console.log('Cannot get data'),
     });
 }
+
 var allData = [];
 function chooseRecipe(recipe) {
     allData = recipe;
@@ -29,88 +48,64 @@ function chooseRecipe(recipe) {
     $('#recipe').append(option);
     $('#css').hide();
 }
+$('#action').hide();
 
+// function for loop data from array variable
+var oldGuests;
 function eachRecipe(id) {
     allData.forEach(item => {
         if (item.id == id) {
-            var step = item.instructions;
-            var cutStep = step.split("<step>");
-            // showScutSteptep
-            result = "";
-            for (var i = 1; i < cutStep.length; i++) {
-                result += `
-                <p class="text-primary">Step ${i}</p>
-                <p>${cutStep[i]}</p>
-            `;
-                $("#instruction_project").html(result);
-                $('#css').show();
-            }
-
-            // showRecipe
-            showRecipe(item.name, item.iconUrl, item.nbGuests);
-
+            recipeimage(item.name,item.iconUrl);
+            //step
+            getStep(item.instructions);
             // showIngredient
             showIngredient(item.ingredients);
+            $('#action').show();
+            $('#member_project').val(item.nbGuests);
+            oldGuests = $('#member_project').val();
+            
         }
     });
 }
 
-// showRecipe
-function showRecipe(name, img, nbGuests) {
-    var result = "";
-    result += `
-    <div class="row">
-    <div class="col-3"></div>
-    <div class="col-3"><h3>${name}</h3></div>
-    <div class="col-3"><img src="${img}"width="100"></div>
-    <div class="col-3"></div>
-    </div>
-    
-    <div class="container mt-5">
-    <div class="row">
-        <div class="col-2"></div>
-        <div class="col-4">
-            <h5>Number of persons</h5>
-        </div>
-        <div class="col-4">
-        <div class="input-group mb-3">
-        <div class="input-group-prepend">
-            <button type="button" id="minus">&minus;</button>
-        </div>
-        <input type="number" class="form-control text-center" value="${nbGuests}" disabled id="member_project" max="15"
-            min="0">
-        <div class="input-group-append">
-            <button type="button" id="add">&#x2b;</button>
-        </div>
-        </div>
-        <div class="col-2"></div>
-    </div>
-</div>
+function updateRecipe(seclect,member) {
+    allData.forEach(item => {
+        if (item.id == seclect) {
+            recipeimage(item.name,item.iconUrl);
+            //step
+            getStep(item.instructions);
+            // showIngredient
+            updateIngredient(item.ingredients,member);
+            $('#action').show();
+        }
+    });
+}
 
-    <div class="container mt-5">
-    <div class="row">
-        <div class="col-2"></div>
-        <div class="col-4">
-            <h5 class="text-center">Ingredients</h5></div>
-        <div class="col-4">
-            <h5>Instructions</h5>
-        </div>
-        <div class="col-2"></div>
-    </div>
-    </div>
+//recipeimage
+function recipeimage(name, image){
+
+    var recipe = "";
+    recipe += `
+    ${name}
+    <img src="${image}" width = "100">
+  
     `;
-    $('#recipe_project').html(result);
+    $('#recipes').html(recipe);
+}
 
-    // function click on icon plus
-    $("#add").on('click', function () {
-        var num = parseInt($("#member_project").val());
-        add(num);
-    });
-    // function click on icon minus
-    $("#minus").on('click', function () {
-        var num = parseInt($("#member_project").val());
-        minus(num);
-    });
+//step
+function getStep(step) {
+    var cutStep = step.split("<step>");
+    // showScutSteptep
+    result = "";
+    for (var i = 1; i < cutStep.length; i++) {
+        result += `
+        <p class="text-primary">Step ${i}</p>
+        <p>${cutStep[i]}</p>
+    `;
+        $("#instruction_project").html(result);
+        $('#css').show();
+    }
 }
 
 // get ingrediant
@@ -129,20 +124,34 @@ function showIngredient(ing) {
     $('#ingradiants_project').html(ingredient);
 }
 
+function updateIngredient(ing,member) {
+    var ingredient = "";
+    ing.forEach(item => {
+        var update = item.quantity * parseInt(member) /oldGuests;
+        ingredient += `
+        <tr>
+            <td><img src="${item.iconUrl}" width="25" class="img-fluid"></td>
+            <td>${update}</td>
+            <td>${item.unit[0]}</td>
+            <td>${item.name}</td>
+        </tr>
+        `;
+    });
+    $('#ingradiants_project').html(ingredient);
+}
 
-// increase value when click on icon sum
+// increase value when click on icon add
 function add(num) {
-    var numAdd = parseInt(num) +1;
-    if(numAdd <= 15) {
+    var numAdd = parseInt(num) + 1;
+    if (numAdd <= 15) {
         $("#member_project").val(numAdd);
-        // getGuests($("#member_project").val());
     }
 }
+
 // decrease value when click on icon minus
 function minus(num) {
-    var minus = parseInt(num)-1;
-    if(minus >= 1) {
+    var minus = parseInt(num) - 1;
+    if (minus >= 0) {
         $("#member_project").val(minus);
-        // getGuests($("#member_project").val());
     }
 }
